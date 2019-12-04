20e44eb80171526ee47e31c141525490d2354853
Jakub Levý <jakub.levy15@gmail.com>
2019-12-04 15:06:33 +0100

    DB error connecting message added

--- Weby/zapoc/db.php
+++ Weby/zapoc/db.php
@@ -1,7 +1,12 @@
 <?php
 include('db_config.php');
 
-$dbh = new PDO("mysql:host=${db_config['server']};dbname=${db_config['database']}", $db_config['login'], $db_config['password']);
-$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);  //make sure that PDO::lastInsertId works
-$GLOBALS['_DBH'] = $dbh;
+try {
+    $dbh = new PDO("mysql:host=${db_config['server']};dbname=${db_config['database']}", $db_config['login'], $db_config['password']);
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);  //make sure that PDO::lastInsertId works
+    $GLOBALS['_DBH'] = $dbh;
+}
+catch (PDOException $e) {
+    die("Could not connect to the DB.");
+}
 ?>
